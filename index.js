ea4a70564dcda977d45f7cadaaaaeb73bb33209c
natalianunes23 <nah.nunes23@gmail.com>
2022-05-26 22:14:01 -0300

    trabalhando com rotas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 const hostname = '127.0.0.1'; //endereço do servidor
 const port = 3000; //porta do servidor
 
@@ -10,4 +10,20 @@ const server = http.createServer((req, res) => {
 
 server.listen(port,hostname, () => {
     console.log(`SERVIDOR RODANDO EM HTTP://${hostname}:${port}/`);
+})*/
+
+const express=require('express');
+const app=express();
+const path=require('path');
+const router=express.Router();
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
 })
+router.get('/about',function(req,res){
+    res.sendFile(path.join(__dirname+'/about.html'));
+})
+
+app.use('/',router);
+app.use('/about',router);
+app.listen(process.env.port||3000);
+console.log("Servidor rodando, graças a Deus!");
